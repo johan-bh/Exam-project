0b6e4754759954fa1d0acd9386365b3216df0244
Johan Böcher Hanehøj <jbhanehoj@gmal.com>
2020-11-18 10:58:30 +0100

    First commit: Project Structure

---
+++ functions.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+df = pd.read_csv("2008.csv")
+print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def load_measurements(filename, fmode):
+
+    return (tvec, data)
+
+
+
+def aggregate_measurements(tvec, data, period):
+
+    return (tvec_a, data_a)
+
+
+
+def print_statistics(tvec, data):
+
+    return
+"""
