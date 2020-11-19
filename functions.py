2efd16aec7d4c0993a22c4e5fade775a8d853470
Johan <jbhanehoj@gmail.com>
2020-11-19 17:07:50 +0100

    feat: agg function draft

--- functions.py
+++ functions.py
@@ -15,7 +15,7 @@ def load_measurements(filename, fmode):
     # Load the data into a pandas DataFrame
     df = pd.read_csv(filename,header=None)
 
-    #df = df.rename(columns={0:'year',1:'month',2:'day',3:'hour',4:'min',5:'sec',6:'zone 1',7:'zone 2',8:'zone 3',9:'zone 4'})
+    df = df.rename(columns={0:'year',1:'month',2:'day',3:'hour',4:'min',5:'sec',6:'zone 1',7:'zone 2',8:'zone 3',9:'zone 4'})
     # Replace all -1 values with NaN
     df = df.replace(to_replace=-1, value=np.nan)
 
@@ -41,21 +41,42 @@ def load_measurements(filename, fmode):
         df = df.dropna()
 
     # Split the DataFrame into a N x 6 time-matrix and a N x 4 data-matrix
-    tvec = df.iloc[:,:-4]
-    data = df.iloc[:,-4:]
+    #tvec = df.iloc[:,:-4]
+    #data = df.iloc[:,-4:]
 
     #df1 = df[df.isna().any(axis=1)]
     #print(df1)
 
-    return (tvec, data)
+    return df
 
-print(load_measurements("2008.csv","drop"))
+#print(load_measurements("2008.csv","drop"))
 
 
 
-def aggregate_measurements(tvec, data, period):
+pd.set_option('display.max_rows', 100)
 
-    return (tvec_a, data_a)
+
+
+def aggregate_measurements(df, period):
+
+    tvec = df.iloc[:,:-4]
+    data = df.iloc[:,-4:]
+
+    if period == "hour":
+        data_a = df.groupby('hour').agg({'zone 1': 'sum','zone 2': 'sum','zone 3': 'sum','zone 4': 'sum'})
+    elif period == "day":
+        data_a = df.groupby('day').agg({'zone 1': 'sum', 'zone 2': 'sum', 'zone 3': 'sum', 'zone 4': 'sum'})
+    elif period == "month":
+        data_a = df.groupby('month').agg({'zone 1': 'sum', 'zone 2': 'sum', 'zone 3': 'sum', 'zone 4': 'sum'})
+    elif period == "hour of the day":
+        data_a = df.groupby('hour').agg({'zone 1': 'mean', 'zone 2': 'mean', 'zone 3': 'mean', 'zone 4': 'mean'})
+
+    ddd = tvec.drop_duplicates(subset=['year','month','day','hour'])
+
+    return data_a
+
+print(aggregate_measurements(load_measurements("2008.csv","drop"),"day"))
+
 
 
 
